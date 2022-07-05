09ea0fdd8861d37b515f1a9a31a9a17c737867aa
Abvdkswm <98737453+Abvdkswm@users.noreply.github.com>
2022-07-05 09:31:37 +0800

    Update and rename plugins/info-bioowner.js to plugin/info-bioowner.js

--- plugin/info-bioowner.js
+++ plugin/info-bioowner.js
@@ -1,24 +1,22 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : Sad-Bot
-*✉️ Nama RL* : Fahri
+*💌 Nama* : Rel Botz
+*✉️ Nama RL* : Farel
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
+*⏰ Tanggal lahir* : 26 April 2007
 *🎨 Umur* : 15
 *🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
-*💔 Benci* : autis, anak epep, seleb
+*🧩 Hobby* : Nonton Hanime, Chatting, nonton bokep, ngocoq, coli
+*💬 Sifat* : Rajin, sopan, baik, tukang kocok
+*🗺️ Tinggal* : Indo, sulteng, kabupaten buol, desa pejako
+*❤️ Suka* : suka sama yang baca>\\\<
+*💔 Benci* : anak ep²
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+*🇫  Facebook* : Farel Rel
+Atau bisa langsung klik: https://www.facebook.com/frel.frel.908132
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
